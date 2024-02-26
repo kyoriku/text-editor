12c64f744073bdb88fb1ede40d5d789ac2402f3e
Austin Graham <austiingraham@gmail.com>
2024-02-25 21:22:53 -0500

    added comments to install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,26 +1,27 @@
+// Getting a reference to the button element with the ID 'buttonInstall'
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Logic for installing the Progressive Web App (PWA)
+// Event handler to the 'beforeinstallprompt' event
 window.addEventListener('beforeinstallprompt', (event) => {
-  window.deferredPrompt = event; // Store the triggered events
-  butInstall.classList.toggle('hidden', false); // Remove the hidden class from the button.
+  window.deferredPrompt = event; // Storing the triggered event for later use
+  butInstall.classList.toggle('hidden', false); // Removing the 'hidden' class from the button to make it visible
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Click event handler on the 'butInstall' element
 butInstall.addEventListener('click', async () => {
   const promptEvent = window.deferredPrompt;
 
-  if (!promptEvent) {
+  if (!promptEvent) { // If there's no deferred prompt available, exit the function
     return;
   }
 
-  promptEvent.prompt(); // Show prompt
-  window.deferredPrompt = null; // Reset the deferred prompt variable, it can only be used once.
-  butInstall.classList.toggle('hidden', true);
+  promptEvent.prompt(); // Display the install prompt to the user
+  window.deferredPrompt = null; // Resetting the deferred prompt variable to null, indicating it has been used
+  butInstall.classList.toggle('hidden', true); // Hiding the install button after the prompt is shown
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Event handler for the 'appinstalled' event
 window.addEventListener('appinstalled', (event) => {
-  window.deferredPrompt = null; // Clear prompt
+  window.deferredPrompt = null; // Clearing the deferred prompt variable after the app is installed
 });
